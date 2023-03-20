24c55d90f43e0a2e4fac785a0d4a84b7c2d67316
Delfian Ruly Havatilla <92741209+delfiansteel@users.noreply.github.com>
2023-03-20 21:17:59 +0700

    Add files via upload

---
+++ php_dasar1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Dasar</title>
+</head>
+
+<body>
+    <h1>Belajar PHP Dasar</h1>
+    <?php
+    echo "Hello World<br>";
+
+    $nim = "312110161";
+    $nama = "Delfian";
+    echo "Nim: " . $nim . "<br>";
+    echo "Nama: " . $nama;
+    ?>
+</body>
+
+</html>
